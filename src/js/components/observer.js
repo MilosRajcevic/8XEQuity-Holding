c2e7f1d971933f0de659b6168c9f44f528248d9f
Milos Rajcevic <rajcevicmilos3@gmail.com>
2022-05-18 23:36:39 +0200

    -Feature intersection observer for active class on navigation links

--- src/js/components/observer.js
+++ src/js/components/observer.js
@@ -1,5 +1,35 @@
 class Observer {
-  init() {}
+  allSections = document.querySelectorAll("[data-section]");
+
+  init() {
+    if (window.screen.width > 1100) this._observer();
+  }
+
+  _intersectingObserver(entries, observer) {
+    const [entry] = entries;
+    const intersectionSectionAtt = entry.target.getAttribute("data-section");
+    const allNavItems = document.querySelectorAll("[data-nav]");
+
+    allNavItems.forEach((item) => {
+      const itemNavAtt = item.getAttribute("data-nav");
+      item.classList.remove("active");
+      if (intersectionSectionAtt === itemNavAtt) item.classList.add("active");
+    });
+  }
+
+  _observer() {
+    const sectionObserver = new IntersectionObserver(
+      this._intersectingObserver,
+      {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.6,
+      }
+    );
+    this.allSections.forEach((section) => {
+      sectionObserver.observe(section);
+    });
+  }
 }
 
 export default Observer;
