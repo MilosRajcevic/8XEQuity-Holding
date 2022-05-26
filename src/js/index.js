37d0f1aa4176548cb99aa0639cbbd313507f5cc8
Milos Rajcevic <rajcevicmilos3@gmail.com>
2022-05-26 11:23:24 +0200

    -Removing Swiper library and Contact section changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,9 @@
 import Navigation from "./components/navigation";
 import HrefScrollSmooth from "./components/hrefScroll";
-import Slider from "./components/slider";
 import Observer from "./components/observer";
 import LazyLoadImages from "./components/lazyLoadImages";
 
 function init() {
-  new Slider().init();
   new Navigation().init();
   new HrefScrollSmooth().init();
   new Observer().init();
